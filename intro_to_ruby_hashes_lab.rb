b10853207ce9044a34b26df72b84ab337f570a9b
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-26 05:10:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,12 @@ def monopoly_with_second_tier
   # complexity.
 
   new_hash = {
-    :railroads => {:pieces => 4},
-    :rent_in_dollars => {}
+    :railroads => {:pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
   }
-
+  }
+  
 end
 
 def monopoly_with_third_tier
